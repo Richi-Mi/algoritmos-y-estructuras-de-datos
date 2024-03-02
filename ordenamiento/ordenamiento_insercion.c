846a0e57edd4b767b6b597142c0a1fac4989f696
Jose Ricardo <joseanakin@outlook.es>
2024-03-01 21:51:52 -0600

    Punteros terminado

--- ordenamiento/ordenamiento_insercion.c
+++ ordenamiento/ordenamiento_insercion.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void ordenamientoPorInsercion( int arr[] ) {
+	int j, key, i;
+	for(j = 1; j < 10; j++){
+                key = arr[j];
+                i = j - 1;
+
+                while( i >= 0 && arr[i] > key ){
+                        arr[i + 1] = arr[i];
+                        i = i - 1;
+                }
+                arr[i+1] = key;
+        }
+}
+
 void main() {
 	// Datos a ordenar
-	int arr[10] = {2, 4, 6, 8, 1, 3, 5, 7, 9, 10};
-	int j, key, i;
+        int arr[10] = {2, 4, 6, 8, 1, 3, 5, 7, 9, 10};
+	int j;
 	
 	printf("Datos desordenados: \n");
 	for( j = 0; j < 10; j++) {
 		printf("%d, ", arr[j]);
 	}
-	
-	for(j = 1; j < 10; j++){
-		key = arr[j];
-		i = j - 1;
 
-		while( i >= 0 && arr[i] > key ){
-			arr[i + 1] = arr[i];
-			i = i - 1;
-		}
-		arr[i+1] = key;
-	}
+	ordenamientoPorInsercion(arr);
+
 	printf("\nDatos Ordenados: \n");
 	for( j = 0; j < 10; j++) {
                 printf("%d, ", arr[j]);
