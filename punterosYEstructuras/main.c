0ee5c73c1ef3ccea0ad8a4594151dd3c294b1d5f
Jose Ricardo <joseanakin@outlook.es>
2024-03-02 16:44:11 -0600

    Punteros y  estrcuturas agregado

---
+++ punterosYEstructuras/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Domicilio
+{
+    char calle[80];
+    int mz;
+    int lote;
+};
+
+
+struct Persona
+{
+    char nombre[80];
+    int edad;
+    struct Domicilio *domicilio;
+};
+
+void menu() {
+    
+}
+
+void main() {
+    
+
+}
