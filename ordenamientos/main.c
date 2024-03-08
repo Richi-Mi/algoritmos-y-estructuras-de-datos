b56f7a57228bf93c461f07bbaaf76ad1a6f14b4e
Jose Ricardo <joseanakin@outlook.es>
2024-03-08 14:05:42 -0600

    Ordenamiento por mezcla terminado

--- ordenamientos/main.c
+++ ordenamientos/main.c
@@ -90,25 +90,65 @@ void ordenamientoSeleccion( struct Vector *vector ) {
     }
 
 }
-void merge( int* arreglo, int p, int q, int r ) {
-    int *n1, *n2, *arrL, *arrR, *i, *j, *k;
-    n1 = malloc( sizeof( int )); 
+void merge(int *arr, int p, int q, int r) {
+    int i, j, k;
+    int n1 = q - p + 1;
+    int n2 = r - q;
 
+    // Crear arrays temporales
+    int L[n1], R[n2];
+
+    // Copiar datos a los arrays temporales L[] y R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[q + 1 + j];
+
+    // Combinar los arrays temporales de vuelta en arr[p..r]
+    i = 0;
+    j = 0;
+    k = p;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copia los elementos restantes de L[]
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copiar los elementos restantes de R[]
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
-void mergeSort( int* arreglo, int p, int r ) {
-    if( p < r ) {
-        int q = ( p + r ) / 2;
-        mergeSort( arreglo, p, q );
-        mergeSort( arreglo, q + 1, r);
-        merge( arreglo, p, q, r );
+
+void mergeSort(int *arr, int p, int r) {
+    if (p < r) {
+        // Encuentra el punto medio del arreglo
+        int q = p + (r - p) / 2;
+
+        // Ordena la primera y segunda mitad del array
+        mergeSort(arr, p, q);
+        mergeSort(arr, q + 1, r);
+
+        // Combina las mitades ordenadas
+        merge(arr, p, q, r);
     }
 }
 void ordenamientoMezcla( struct Vector *vector ) {
-    //TODO Programar alogritmo cuando lo explique la profa
-    int n, q;
-    q = (vector -> tamaño) / 2;
-
-
+    mergeSort( vector -> arreglo, 0, vector -> tamaño - 1 );
 }
 
 void showMenu(int *bandera)
