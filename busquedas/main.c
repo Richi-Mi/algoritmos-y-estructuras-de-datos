2bc16da904adcc8bd54c6cca3518913a9a604452
Hector David <tetuanorg@gmail.com>
2024-03-14 07:55:22 -0600

    Que me dice que me quiere

---
+++ busquedas/main.c
@@ -0,0 +1,57 @@
+/**
+ * Que me dice que me quiere
+Que vivir sin mí no puede
+Que lo agobian los momentos en que no estoy junto a él
+Que siempre me había esperado
+Que era a quien había soñado
+La que su mamá quería pa' que fuera su mujer
+Que mis ojos son estrellas
+Que mi risa es la más bella
+Que todos mis atributos son perfectos para él
+Que si fuera un retratista
+Que si fuera un buen artista
+Yo sería su Mona Lisa y hasta un tango de Gardel
+Y a él solo lo trago yo
+Quiero que tú sepas que tú no eres para mí
+Que tú no eres para mí, siempre supe pero no hice caso
+Que ni se te ocurra aparecer por aquí
+Aparecer por aquí
+Con tus enredos y cuentos baratos
+Quiero que tú sepas que yo no soy para ti
+Que no yo soy para ti, siempre supe pero no hice caso
+Que ni se me ocurra estar de nuevo junto a ti
+Estar de nuevo junto a ti
+Mi corazón no aguanta más fracaso
+Que me promete la luna
+Que yo soy como ninguna
+Que parezco una doncella de esas que hay que proteger
+Que sus vicios ha dejado
+Que su suelo le ha aumentado
+Que me promete la vida que yo siento merecer
+Y hoy solo lo trago yo
+Quiero que tú sepas que tú no eres para mí
+Que tú no eres para mí, siempre supe pero no hice caso
+Que ni se te ocurra aparecer por aquí
+Aparecer por aquí
+Con tus enredos y cuentos baratos
+Quiero que tú sepas que yo no soy para ti
+Que no yo soy para ti, siempre supe pero no hice caso
+Que ni se me ocurra estar de nuevo junto a ti
+Estar de nuevo junto a ti
+Mi corazón no aguanta más fracaso
+Y yo que tanto me decía que no me convenías
+Que eras mi destrucción
+Y siempre yo tratando de dejarte
+Tu día ya llego
+Tu día ya llego
+Quiero que tú sepas que tú no eres para mí
+Que tú no eres para mí, siempre supe pero no hice caso
+Que ni se te ocurra aparecer por aquí
+Aparecer por aquí
+Con tus enredos y cuentos baratos
+Quiero que tú sepas que yo no soy para ti
+Que no yo soy para ti, siempre supe pero no hice caso
+Que ni se me ocurra estar de nuevo junto a ti
+Estar de nuevo junto a ti
+Mi corazón no aguanta más fracaso
+*/
