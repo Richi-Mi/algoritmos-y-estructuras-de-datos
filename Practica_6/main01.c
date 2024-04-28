b87ea3cfd300b6211c60db8406140dae5f2a9bd1
Hector David <tetuanorg@gmail.com>
2024-04-28 11:24:43 -0600

    Practica_6 Dinamica

--- Practica_6/main01.c
+++ Practica_6/main01.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 typedef struct Nodo{
 
-    int valor;
+    float valor;
     struct Nodo *sig;
 
 }Nodo;
 
-// Apuntador doble
-void agregar(Nodo **p, int x){
+void agregar(Nodo **p, float x){
 
     Nodo *new = (Nodo *)malloc(sizeof(Nodo));
 
@@ -51,26 +51,180 @@ void mostrar(Nodo *p){
         aux = aux->sig;
     }
 
-    printf("Tetuan...");
     printf("\n");
 }
 
+Nodo* insertOrdenado(Nodo **p, float v){
+
+    Nodo *new = (Nodo*)malloc(sizeof(Nodo));
+
+    new->valor = v;
+    new->sig = NULL;
+
+    Nodo *aux = *p;     
+    Nodo *ant = NULL;  
+
+    while( aux != NULL && aux->valor < v ){
+
+        ant = aux;
+        aux = aux->sig;
+
+    }
+
+    if(ant != NULL)
+        ant->sig = new;
+    
+    else
+        *p = new;
+    
+    new->sig = aux;
+
+    return new;
+
+}
+
+Nodo* listaOrdenada(Nodo **p){
+
+    Nodo *nuevo = NULL;
+    Nodo *aux = *p;
+
+    while(aux != NULL){
+        insertOrdenado(&nuevo,aux->valor);
+        aux = aux->sig;
+    }
+
+    return nuevo;
+
+}
+
+void totalValores(Nodo *p, float *cant){
+
+    *(cant) = 0;
+
+    while(p != NULL){
+        (*cant)++;
+        p = p->sig;
+    }
+
+}
+
+void media(Nodo *p, float cant, float *media){
+
+    float sumaNum;
+
+    while(p != NULL){
+        sumaNum += p->valor;
+        p = p->sig;
+    }
+
+    *(media) = sumaNum/cant;
+
+}
+
+void mediana(Nodo *p, int cant, float *mediana){
+
+    Nodo *aux = p;
+    int mid = cant/2;
+    int i=0;
+
+    while(p != NULL){
+        i++;
+        p = p->sig;
+
+        if(!(cant%2)){//Pares
+
+            if(i == mid-1){
+        
+                *(mediana) = p->valor;
+                p=p->sig;
+                *(mediana) += p->valor;
+                *(mediana) /= 2;
+                break;
+
+            }
+
+        }
+        else{         //Impares
+    
+            if(i == mid){
+        
+                *(mediana) = p->valor;
+                break;
+        
+            }
+
+        }
+
+
+    }
+
+}
+
+int moda(){
+
+}
+
+void varinza(Nodo *p, float cant, float med, float *varz){
+
+    while(p != NULL){
+        *(varz) += pow((p->valor-med),2);
+        p = p->sig;
+    }
+
+    *(varz) /= cant;
+
+}
+
+void desviaciónEstandar(Nodo *p, float cant, float med, float *devStand){
+
+    while(p != NULL){
+        *(devStand) += pow((p->valor-med),2);
+        p = p->sig;
+    }
+
+    *(devStand) = sqrtf(*(devStand) / cant);
+
+}
+
 int main(){
-
+    
     Nodo *p = NULL;
+    Nodo *new = NULL;
     int x;
+    float cant, med, median, varz, devEstandar;
 
-    printf("Escriba numeros para hacer operaciones (Al escribir 0 se detiene)");
+    printf("Escriba numeros para hacer operaciones (Al escribir 0 se detiene)\n");
 
-    while(!x){
+    while(1){
 
         scanf("%d",&x);
+
+        if(!x)
+            break;
 
         agregar(&p,x);
 
     }
 
+    printf("Lista desordenada:\n");
     mostrar(p);
+
+
+    printf("Lista ordenada:\n");
+    new = listaOrdenada(&p);
+    mostrar(new);
+
+    totalValores(new,&cant);
+    media(new,cant,&med);
+    mediana(new,cant,&median);
+    varinza(p,cant,med,&varz);
+    desviaciónEstandar(p,cant,med,&devEstandar);
+
+    printf("\nTotal de valores: %d",cant);
+    printf("\nMedia: %0.1f",med);
+    printf("\nMediana: %0.2f",median);
+    printf("\nVarianza: %0.2f",varz);
+    printf("\nDesviacion estandar: %0.2f",devEstandar);
 
     free(p);
 
--- Punteros_Y_Estructuras/Pilas.c
+++ Punteros_Y_Estructuras/Pilas.c
@@ -3,10 +3,68 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define Max 3
 
+
+//Comprobar si pila esta llena
+int full(int tope){
+
+    if(tope == Max-1)
+        return -1;
+    
+    else
+        return 0;
+
+}
+
+//Comprobar si pila esta vacia
+int empty(int tope){
+
+    if(tope == -1)
+        return 1;
+    
+    else
+        return 0;
+
+}
+
+//Agregar valores en una pila
+void push(int pila[], int *tope, int v){
+
+    if(!full(*tope))
+        pila[(*tope)++] = v;
+
+    else
+        printf("No es posible agregar un elemento\n");
+
+}
+
+//Quitar valor en una pila
+int pop(int pila[], int *tope){
+
+    if(!empty(*tope))
+        return (pila[(*tope)--]); //Envia el valor del tope y después decrementa 'tope'
+
+    else
+        printf("La lista esta vacia");
+
+}
 
 int main(){
 
+    int tope = -1;
+    int pila[Max];
+    int valor;
 
+    push(pila,&tope,1);
+    push(pila,&tope,2);
+    push(pila,&tope,3);
+
+    while(!empty(tope)){
+
+        valor = pop(pila,&tope);
+        printf("%i\n",valor);
+
+    }
 
 }
