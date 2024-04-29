6f1ab09f1eefd0e4ec16b6ee631bac691223bc0e
Jose Ricardo <joseanakin@outlook.es>
2024-04-29 07:43:53 -0600

    Mansaje re_nata otaku

--- Practica_6/main.c
+++ Practica_6/main.c
@@ -25,11 +25,11 @@ int insertToEnd( Persona personas[] );
 
 int insertTo( Persona personas[], Persona personToInsert, int position );
 
-Persona searchPersonByRFC( Persona personas[], char rfc[] );
+int searchPersonByRFC( Persona personas[], char rfc[] );
 
 Persona searchPersonByIndex( Persona personas[], int index );
 
-Persona deletePerson( Persona personas[], int index );
+Persona deletePerson( Persona personas[], char rfc[] );
 
 Persona getDataFromConsole();
 
@@ -38,7 +38,16 @@ void showAllElements( Persona personas[] );
 int main(){
 
     Persona personas[ sizeArr ];
-    int option, result;
+    int option, result, i;
+
+    for( i = 0; i  < sizeArr; i++ ) {
+        Persona emptyPerson;
+        strcpy( emptyPerson.nombre, "-");
+        strcpy( emptyPerson.rfc, "-");
+
+        personas[i] = emptyPerson;
+    }
+
     do {
         printf("\n*---------------------MENU----------------------*\n");
         printf("| 1. Dar de alta a una persona \t\t\t|\n| 2. Buscar a una persona por RFC \t\t|\n| 3. Borrar a una persona por RFC \t\t|\n| 4. Mostrar todos los elementos de la lista \t|\n| 0. Salir \t\t\t\t\t|");
@@ -49,6 +58,13 @@ int main(){
         switch (option){
         case 1:
             result = insertToEnd( personas );
+
+            if( result == 0 ) {
+                printf("Usuario registrado correctamente \n");
+            }
+            else {
+                printf("Se ha alcanzado el numero maximo de usuarios \n");
+            }
             break;
         case 2:
             // Get RFC.
@@ -56,7 +72,7 @@ int main(){
             char rfc[12];
             scanf("%s", rfc );
             
-            Persona foundedPerson = searchPersonByRFC( personas, rfc );
+            Persona foundedPerson = personas[ searchPersonByRFC( personas, rfc ) ];
             printf("Persona encontrada: %s", foundedPerson.nombre );
 
             break;
@@ -100,6 +116,8 @@ int insertToEnd( Persona personas[] ){
     if( personToInsert.nombre == NULL || personToInsert.rfc == NULL ) 
         return -1;
 
+    if( sizeAct >= 10 )
+        return -1;
     // Agregar persona.
     personas[sizeAct] = personToInsert;
     sizeAct++;
@@ -108,15 +126,11 @@ int insertToEnd( Persona personas[] ){
 }
 
 int insertTo(Persona personas[], Persona personToInsert, int position ){
-
+    personas[position] = personToInsert;
 }
 
-Persona searchPersonByRFC(Persona personas[], char rfc[] ){
+int searchPersonByRFC(Persona personas[], char rfc[] ){
     int i, j; 
-
-    Persona emptyPerson;
-
-    strcpy( emptyPerson.nombre, "null");
 
     for( i = 0; i < sizeAct; i++ ) {
         int bandera = 1;
@@ -128,20 +142,29 @@ Persona searchPersonByRFC(Persona personas[], char rfc[] ){
         }
 
         if( bandera == 1 ) {
-            return personas[i];
+            return i;
         } 
     }
-    return emptyPerson;
+    return -1;
 }
 
 Persona searchPersonByIndex(Persona personas[], int index ) {
-
+    return personas[index];
 }
 
 void showAllElements( Persona personas[] ){
     int i; 
 
-    for( i = 0; i < sizeAct; i++ ) {
+    for( i = 0; i < sizeArr; i++ ) {
         printf("%d. - %s con RFC: %s\n", i, personas[i].nombre, personas[i].rfc );
     }
+    printf("La cantidad total de usuarios registrados es: %d", sizeAct );
+}
+Persona deletePerson( Persona personas[], char rfc[] ) {
+    Persona emptyPerson;
+    strcpy( emptyPerson.nombre, "null");
+    strcpy( emptyPerson.rfc, "null");
+
+    int i = searchPersonByRFC( personas, rfc );
+    personas[i] = emptyPerson;
 }
