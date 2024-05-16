2a58f4cd901ea520782f084a895f10760a7bb51c
Hector David <hd.tetuan5002@gmail.com>
2024-05-16 08:27:23 -0600

    Apunte_colaDinamica

--- Cola/cola_dinamica.c
+++ Cola/cola_dinamica.c
@@ -4,19 +4,58 @@
 typedef struct Cola{
     
     int valor;
-    
+    struct Cola *sig;
 
 
 }Cola;
 
-void insertarFinal(){
+void enqueue(Cola **p, int x){
+
+    Cola *nuevo = (Cola*)malloc(sizeof(Cola));
+    nuevo->valor = x;
+
+    if(*p = NULL)
+        nuevo->sig = nuevo;
+    else{
+        nuevo->sig = (*p)->sig;
+        (*p)->sig = nuevo;
+    }
+
+    *p = nuevo;
 
 
+}
+
+int deenqueue(Cola **p){
+
+    int v = (*p)->sig->valor;
+
+    if((*p)->sig == *p){
+        free(*p);
+        (*p) = NULL;
+    }
+
+    else{
+
+        Cola *aux = (*p)->sig;
+
+        (*p)->sig = aux->sig;
+        free(aux);
+
+    }
+
+    return v;
 
 }
 
 
 int main(){
+
+    Cola *p = NULL;
+
+    enqueue(&p,5);
+
+
 
 
 
