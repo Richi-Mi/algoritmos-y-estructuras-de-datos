bf8fe57ba232f1a76383d759334804393e1b11cf
Hector David <hd.tetuan5002@gmail.com>
2024-06-07 09:39:28 -0600

    funciones hash

--- Tablas_Hash/main.c
+++ Tablas_Hash/main.c
@@ -1,9 +1,45 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+typedef struct Tabla{
+    
+    int indice;
+    char nombre;
+    struct tabla *sig;
+    struct tabla *ant;
+
+}tabla;
+
+
+directAddressSearch(tabla *t, int x){
+
+    return (t->indice = x);
+
+}
+
+directAddressInsertS(tabla *t, int x){
+
+}
+
+directAddressDelete(tabla *t, int x){
+
+}
+
+//Busqueda eliminar elementos
+chainedHashInsert(tabla *t, int x){
+    
+}
+
+chainedHashSearch(tabla *t, int x){
+
+}
+
+chainedHashDelete(tabla *t, int x){
+
+}
+
 
 //Aplicación --> busquedas
-
 int main(){
 
 
