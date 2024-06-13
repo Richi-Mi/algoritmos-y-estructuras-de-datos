a62245ba8ef6ff53be30c821233fb26b85876c0d
Jose Ricardo <joseanakin@outlook.es>
2024-06-13 07:17:45 -0600

    P10 semifinished

--- Practica_10/main.c
+++ Practica_10/main.c
@@ -24,19 +24,13 @@ void addCharacter( Letter **start, char c ) {
     Letter *new = malloc( sizeof( Letter ) );
     new -> value = c;
     new -> siguiente = NULL;
+
+    Letter *aux = *start;
+    while( aux -> siguiente != NULL ) {
+        aux = aux -> siguiente;
+    }
+    aux -> siguiente = new;
     
-    // Comprobamos que no existan elementos en el Nodo.
-    if( *start == NULL ) {
-        *start = new;
-    }
-    // Si si hay elementos
-    else {
-        Letter *aux = *start;
-        while( aux -> siguiente != NULL ) {
-            aux = aux -> siguiente;
-        }
-        aux -> siguiente = new;
-    }
 }
 
 //// METODOS DE LISTA DOBLEMENTE ENLAZADA.
@@ -88,6 +82,19 @@ void addElementToEnd( Student **inicio, Student **final, int boleta, Letter *nam
     }
 
 }
+void cleanString( Letter *inicio ) {
+    printf("Usuario Eliminado: ");
+    while( inicio != NULL ) {
+        Letter *aux = inicio;
+
+        inicio = inicio -> siguiente;
+
+        printf("%c", aux -> value );
+
+        free( aux );
+    }
+    printf("\n");
+}
 void deleteStudentByHisBoleta( Student **inicio, Student **final, int boleta ) {
     // Creamos el Student auxiliar para poder referenciarlo al eliminarlo.
     Student *aux = *inicio;
@@ -101,6 +108,9 @@ void deleteStudentByHisBoleta( Student **inicio, Student **final, int boleta ) {
     if( aux != NULL ) {
         anterior  = aux -> before;
         siguiente = aux -> next;
+
+        cleanString( aux -> name );
+        cleanString( aux -> lastnames );
 
         // Significa que es el primer elemento a eliminar.
         if( anterior == NULL ) {
@@ -121,13 +131,23 @@ void deleteStudentByHisBoleta( Student **inicio, Student **final, int boleta ) {
         free( aux );
     }
 }
-void showStudents( Student *s ) {
-    Student *aux = s;
-    while ( s != NULL ) {
-        printf("%d, ", s -> boleta );
-        s = s -> next;
+
+void getStringFromConsole( Letter **string ) {
+    char caracter;
+    // Si el puntero a string es NULL, asignar memoria para el primer nodo
+    if (*string == NULL) {
+        *string = malloc(sizeof(Letter));
+        (*string)->siguiente = NULL;
+    }
+    Letter *current = *string;
+    while( ( caracter = getchar() ) != '\n') {
+        addCharacter( &current, caracter );
+        current = current->siguiente;
     }
 }
+
+
+// METODOS PARA PODER MOSTRAR LA INFORMACIÓN.
 void showString( Letter *inicio ) {
     Letter *aux = inicio;
     while( aux != NULL ) {
@@ -135,6 +155,18 @@ void showString( Letter *inicio ) {
         aux = aux -> siguiente;
     }
 }
+void showStudents( Student *s ) {
+    Student *aux = s;
+    while ( s != NULL ) {
+        printf("\n Estudiante: ");
+        showString( s -> name );
+        printf(" ");
+        showString( s -> lastnames );
+        printf("\n Boleta: %d \n", s -> boleta );
+        s = s -> next;
+    }
+}
+
 int main( void ) {
     // 1. Inicilaizamos el inicio y final de la lista.
     Student *inicio, *final;
@@ -142,16 +174,6 @@ int main( void ) {
 
     inicio = NULL;
     final  = NULL;
-
-    Letter *c = malloc( sizeof( Letter ) );
-    c -> value = 'c';
-    c -> siguiente = NULL;
-
-    char l;
-
-    while ( (l = getchar()) != '\n' ) {
-        addCharacter( &c, l );               
-    }
 
     printf("* ---------------------- *\n");
     printf("| SAES 2.0 versión ESCOM |\n");
@@ -163,26 +185,51 @@ int main( void ) {
         printf("\t - > 2. Mostrar alumnos dados de alta. \n");
         printf("\t - > 3. Modificar registro de un alumno. \n");
         printf("\t - > 4. Eliminar a un alumno (p/boleta) \n");
-        printf("\t - > 0. Salir \n");
+        printf("\t - > 0. Salir \n\n");
 
         scanf("%d", &opt );
 
         fflush( stdin );
 
+        // Dar de alta un alumno.
         if( opt == 1 ) {
 
-            addElementToStart( &inicio, &final, 2024 + i, c, c );
+            Letter *name = NULL;
+            Letter *lastnames = NULL;
+            int boleta;
+
+            getchar(); // Para tomar el ultimo '\n'
+
+            printf("Ingrese el nombre del alumno: ");
+            getStringFromConsole( &name );
+            printf("\n");
+
+            printf("Ingrese los apellidos del alumno: ");
+            getStringFromConsole( &lastnames );
+            printf("\n");
+
+            printf("Ingrese la boleta del alumno: ");
+            scanf("%d", &boleta);
+            printf("\n");
+
+            addElementToStart( &inicio, &final, boleta, name, lastnames );
+
             i++;
+            fflush( stdin );
         }
         if( opt == 2 ) {
-            showString( c );
             showStudents( inicio );
         }
         if( opt == 3 ) {
 
         }
         if( opt == 4 ) {
+            int boleta;
+            printf("Ingrese la boleta: ");
+            scanf("%d", &boleta );
+            printf("\n");
 
+            deleteStudentByHisBoleta( &inicio, &final, boleta );
         }
     } while ( opt != 0 );
     return 0;
