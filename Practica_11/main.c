76be84b1277c22e44bc61922779a59074f5cac07
Hector David <hd.tetuan5002@gmail.com>
2024-06-17 07:59:20 -0600

    Begin Pract_11

---
+++ Practica_11/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define m 30;
+
+typedef struct Nodo{
+    
+    int index;
+    int clave;
+    struct Nodo *sig;
+
+}Nodo;
+
+
+int metodoDivision(int k, int m){
+    int h = k % m;
+}
+
+int main(){
+
+
+}
